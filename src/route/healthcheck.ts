047dc880c3a27af7c0aa58cc31663dbbf12478c6
Phil Moorhouse <phil@wheelsandcogs.co.uk>
2024-09-10 17:05:17 +0100

    ignore datalake connection for now

--- src/route/healthcheck.ts
+++ src/route/healthcheck.ts
@@ -1,12 +1,11 @@
 import { Router } from 'express';
 import passport from 'passport';
 
-import { logger } from '../utils/logger';
-import { DataLakeService } from '../controllers/datalake';
+// import { logger } from '../utils/logger';
+// import { DataLakeService } from '../controllers/datalake';
 import { sanitiseUser } from '../utils/sanitise-user';
 import { User } from '../entities/user';
 
-const dataLakeService = new DataLakeService();
 const healthcheck = Router();
 
 healthcheck.get('/basic', (req, res) => {
@@ -17,15 +16,16 @@ healthcheck.get('/jwt', passport.authenticate('jwt', { session: false }), (req, 
     res.json({ message: 'success', user: sanitiseUser(req.user as User) });
 });
 
-healthcheck.get('/datalake', (req, res) => {
-    try {
-        dataLakeService.listFiles();
-    } catch (err) {
-        logger.error(`Unable to connect to datalake: ${err}`);
-        res.status(500).json({ message: 'error' });
-        return;
-    }
-    res.json({ message: 'success' });
-});
+// healthcheck.get('/datalake', (req, res) => {
+//     const dataLakeService = new DataLakeService();
+//     try {
+//         dataLakeService.listFiles();
+//     } catch (err) {
+//         logger.error(`Unable to connect to datalake: ${err}`);
+//         res.status(500).json({ message: 'error' });
+//         return;
+//     }
+//     res.json({ message: 'success' });
+// });
 
 export const healthcheckRouter = healthcheck;
