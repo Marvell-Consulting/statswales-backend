d8c39c5ac719007c44d2b8d51bf2b07fe0bf5cf3
Phil Moorhouse <phil@wheelsandcogs.co.uk>
2024-12-11 17:18:53 +0000

    working export

--- src/route/translation.ts
+++ src/route/translation.ts
@@ -1,39 +1,77 @@
 import { Request, Response, NextFunction, Router } from 'express';
+import { stringify } from 'csv';
 
 import { logger } from '../utils/logger';
 import { UnknownException } from '../exceptions/unknown.exception';
+import { Dataset } from '../entities/dataset/dataset';
+import { DatasetInfo } from '../entities/dataset/dataset-info';
+import { TranslationDTO } from '../dtos/translations-dto';
+
+import { loadDataset } from './dataset';
+
+const collectTranslations = (dataset: Dataset): TranslationDTO[] => {
+    const metadataEN = dataset.datasetInfo?.find((info) => info.language.includes('en'));
+    const metadataCY = dataset.datasetInfo?.find((info) => info.language.includes('cy'));
+
+    const metadataProps: (keyof DatasetInfo)[] = [
+        'title',
+        'description',
+        'collection',
+        'quality',
+        'roundingDescription'
+    ];
+
+    const translations: TranslationDTO[] = [
+        ...dataset.dimensions?.map((dim) => ({
+            type: 'dimension',
+            id: dim.id,
+            key: dim.factTableColumn,
+            english: dim.dimensionInfo?.find((info) => info.language.includes('en'))?.name,
+            welsh: dim.dimensionInfo?.find((info) => info.language.includes('cy'))?.name
+        })),
+        ...metadataProps.map((prop) => ({
+            type: 'metadata',
+            key: prop,
+            english: metadataEN?.[prop] as string,
+            welsh: metadataCY?.[prop] as string
+        }))
+    ];
+
+    return translations;
+};
 
 export const translationRouter = Router();
 
-translationRouter.get('/preview', async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        logger.info('Previewing translations for export...');
-
-        // collect all the things needing translation
-        // return it as a json object
-
-        res.json({});
-    } catch (error) {
-        logger.error('Error previewing translations', error);
-        next(new UnknownException());
+translationRouter.get(
+    '/:dataset_id/preview',
+    loadDataset(),
+    async (req: Request, res: Response, next: NextFunction) => {
+        try {
+            logger.info('Previewing translations for export...');
+            const dataset: Dataset = res.locals.dataset;
+            const translations = collectTranslations(dataset);
+            res.json(translations);
+        } catch (error) {
+            logger.error('Error previewing translations', error);
+            next(new UnknownException());
+        }
     }
-});
+);
 
-translationRouter.get('/export', async (req: Request, res: Response, next: NextFunction) => {
+translationRouter.get('/:dataset_id/export', loadDataset(), async (req: Request, res: Response, next: NextFunction) => {
     try {
         logger.info('Exporting translations to CSV...');
-
-        // collect all the things needing translation
-        // return it as a CSV file
-
-        res.json({});
+        const dataset: Dataset = res.locals.dataset;
+        const translations = collectTranslations(dataset);
+        res.setHeader('Content-Type', 'text/csv');
+        stringify(translations, { bom: true, header: true }).pipe(res);
     } catch (error) {
         logger.error('Error exporting translations', error);
         next(new UnknownException());
     }
 });
 
-translationRouter.get('/import', async (req: Request, res: Response, next: NextFunction) => {
+translationRouter.get('/:dataset_id/import', loadDataset(), async (req: Request, res: Response, next: NextFunction) => {
     try {
         logger.info('Importing translations from CSV...');
 
