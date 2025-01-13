13352c26504050937513c517bc9ecc3105f6a390
Phil Moorhouse <phil@wheelsandcogs.co.uk>
2025-01-13 12:08:24 +0000

    bypass join table for dataset topics in response

--- src/dtos/dataset-topic-dto.ts
+++ src/dtos/dataset-topic-dto.ts
@@ -1,15 +1,20 @@
 import { DatasetTopic } from '../entities/dataset/dataset-topic';
 
+import { TopicDTO } from './topic-dto';
+
 export class DatasetTopicDTO {
     id: string;
     dataset_id: string;
     topic_id: number;
+    topic: TopicDTO;
 
     static fromDatasetTopic(datasetTopic: DatasetTopic): DatasetTopicDTO {
         const dto = new DatasetTopicDTO();
         dto.id = datasetTopic.id;
         dto.dataset_id = datasetTopic.datasetId;
         dto.topic_id = datasetTopic.topicId;
+        dto.topic = TopicDTO.fromTopic(datasetTopic.topic);
+
         return dto;
     }
 }
