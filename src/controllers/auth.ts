78e1170f6c207af3cde4a222e66d09d45bc0aa76
Phil Moorhouse <phil@wheelsandcogs.co.uk>
2025-01-30 21:56:25 +0000

    add support for simple form login (TEMPORARY)

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -3,16 +3,50 @@ import { URL } from 'node:url';
 import { RequestHandler } from 'express';
 import passport from 'passport';
 import jwt from 'jsonwebtoken';
+import { Repository } from 'typeorm';
 
 import { appConfig } from '../config';
 import { logger } from '../utils/logger';
 import { User } from '../entities/user/user';
 import { sanitiseUser } from '../utils/sanitise-user';
 import { AuthProvider } from '../enums/auth-providers';
+import { dataSource } from '../db/data-source';
 
 const config = appConfig();
 const domain = new URL(config.auth.jwt.cookieDomain).hostname;
 logger.debug(`JWT cookie domain is '${domain}'`);
+
+// TODO: remove once EntraID is available for WG users
+export const loginLocal: RequestHandler = async (req, res, next) => {
+    logger.debug('auth request from local form received');
+
+    const returnURL = `${config.frontend.url}/auth/callback`;
+    const username = ((req.query.username as string) || '').trim();
+
+    if (!username) {
+        logger.error('local auth failed: username must be provided');
+        res.redirect(`${returnURL}?error=login`);
+        return;
+    }
+
+    try {
+        logger.debug('checking if user exists...');
+        const userRepository: Repository<User> = dataSource.getRepository('User');
+        const user = await userRepository.findOneByOrFail({ providerUserId: username, provider: 'local' });
+        logger.debug('existing user found');
+
+        logger.info('local auth successful, creating JWT and returning user to the frontend');
+        const payload = { user: sanitiseUser(user) };
+        const { secret, expiresIn, secure } = config.auth.jwt;
+        const token = jwt.sign(payload, secret, { expiresIn });
+
+        res.cookie('jwt', token, { secure, httpOnly: true, domain });
+        res.redirect(returnURL);
+    } catch (error) {
+        logger.error(error);
+        res.redirect(`${returnURL}?error=login`);
+    }
+};
 
 export const loginGoogle: RequestHandler = (req, res, next) => {
     logger.debug('attempting to authenticate with Google...');
