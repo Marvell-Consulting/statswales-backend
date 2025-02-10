2146f85f04fe18a2262be1a01170f7945c93890e
Phil Moorhouse <phil@wheelsandcogs.co.uk>
2025-02-10 15:48:49 +0000

    add Welsh translations for topics

--- src/seeders/topics.ts
+++ src/seeders/topics.ts
@@ -8,8 +8,12 @@ import { logger } from '../utils/logger';
 import { Topic } from '../entities/dataset/topic';
 
 interface CSVRow {
-    L1: string;
-    L2: string;
+    id: string;
+    path: string;
+    l1_en: string;
+    l1_cy: string;
+    l2_en: string;
+    l2_cy: string;
 }
 
 export default class TopicSeeder extends Seeder {
@@ -21,40 +25,23 @@ export default class TopicSeeder extends Seeder {
         const em = dataSource.createEntityManager();
         const csv = `${__dirname}/../resources/topics/topics.csv`;
         const parserOpts = { delimiter: ',', bom: true, skip_empty_lines: true, columns: true };
-        const rootTopics: Topic[] = [];
-        const childTopics: Topic[] = [];
-        let topicId = 1;
+        const topics: Topic[] = [];
 
         const parseCSV = async () => {
             const csvParser: AsyncIterable<CSVRow> = fs.createReadStream(csv).pipe(parse(parserOpts));
-            let rootTopic = Topic.create();
 
             for await (const row of csvParser) {
-                if (row.L1) {
-                    rootTopic = Topic.create();
-                    rootTopic.id = topicId;
-                    rootTopic.path = topicId.toString();
-                    rootTopic.nameEN = row.L1.trim();
-                    rootTopics.push(rootTopic);
-                    topicId++;
-                    await em.save<Topic>(rootTopic);
-                    logger.info(`Seeded root topic '${rootTopic.nameEN}'`);
-                }
-
-                if (row.L2) {
-                    const childTopic = Topic.create();
-                    childTopic.id = topicId;
-                    childTopic.path = `${rootTopic.path}.${topicId}`;
-                    childTopic.nameEN = row.L2.trim();
-                    childTopics.push(childTopic);
-                    topicId++;
-                    await em.save<Topic>(childTopic);
-                    logger.info(`Seeded child topic '${childTopic.nameEN}'`);
-                }
+                const topic = new Topic();
+                topic.id = parseInt(row.id, 10);
+                topic.path = row.path;
+                topic.nameEN = row.id === row.path ? row.l1_en : row.l2_en;
+                topic.nameCY = row.id === row.path ? row.l1_cy : row.l2_cy;
+                topics.push(topic);
             }
+            await em.save(Topic, topics);
         };
 
         await parseCSV();
-        logger.info(`Seeded ${rootTopics.length} root topics and ${childTopics.length} child topics`);
+        logger.info(`Seeded ${topics.length} topics`);
     }
 }
