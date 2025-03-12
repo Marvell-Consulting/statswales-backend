787c0f004a873b8b86cc911efec266d9abe22f3c
Phil Moorhouse <phil@wheelsandcogs.co.uk>
2025-03-12 15:32:34 +0000

    re-add prettier eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 // @ts-check
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 
 export default tseslint.config(
   [
@@ -18,6 +19,7 @@ export default tseslint.config(
     },
     eslint.configs.recommended,
     tseslint.configs.recommended,
+    eslintPluginPrettierRecommended,
     {
       rules: {
         'no-console': 'error',
