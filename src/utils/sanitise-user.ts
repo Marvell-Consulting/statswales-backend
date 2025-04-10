e5646144c6d8ebba081b0aec8c17bb883ee22a21
Phil Moorhouse <phil@wheelsandcogs.co.uk>
2025-04-10 16:23:49 +0100

    replace sanitiseUser with UserDTO

--- src/utils/sanitise-user.ts
+++
@@ -1,8 +0,0 @@
-import { pick } from 'lodash';
-
-import { User } from '../entities/user/user';
-
-// strip anything from the user object that we do not want to expose to the client
-export const sanitiseUser = (user: User): Partial<User> => {
-  return pick(user, ['id', 'email', 'givenName', 'familyName']);
-};
