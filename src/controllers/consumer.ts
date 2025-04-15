a26a78f5e2c6d1ac4022f86d4d4781db8273b564
Jamie Maynard <29251905+j-maynard@users.noreply.github.com>
2025-04-15 16:47:27 +0100

    Implement consumer data view

--- src/controllers/consumer.ts
+++ src/controllers/consumer.ts
@@ -15,6 +15,7 @@ import fs from 'node:fs';
 import { cleanUpCube } from '../services/cube-handler';
 import { outputCube } from './cube-controller';
 import { DuckdbOutputType } from '../enums/duckdb-outputs';
+import {getTableRowsNoFilterNoSort} from "../services/consumer-view";
 
 export const listPublishedDatasets = async (req: Request, res: Response, next: NextFunction) => {
   logger.info('Listing published datasets...');
@@ -39,6 +40,16 @@ export const listPublishedDatasets = async (req: Request, res: Response, next: N
 export const getPublishedDatasetById = async (req: Request, res: Response) => {
   const dataset = await PublishedDatasetRepository.getById(res.locals.datasetId, withAll);
   res.json(ConsumerDatasetDTO.fromDataset(dataset));
+};
+
+export const getPublishedDatasetView = async (req: Request, res: Response) => {
+  const dataset = await PublishedDatasetRepository.getById(res.locals.datasetId, withAll);
+  const lang = req.language.split('-')[0];
+  if (!dataset.publishedRevision) {
+    throw new NotFoundException('errors.no_revision');
+  }
+  const { metadata, rows } = await getTableRowsNoFilterNoSort(dataset.id, dataset.publishedRevision, lang);
+  res.json(rows);
 };
 
 export const downloadPublishedDataset = async (req: Request, res: Response, next: NextFunction) => {
---
+++ src/services/consumer-view.ts
@@ -0,0 +1,31 @@
+import { parquetMetadata, parquetReadObjects } from 'hyparquet';
+import { getFileService } from '../utils/get-file-service';
+import { Revision } from '../entities/dataset/revision';
+
+export const getTableRowsNoFilterNoSort = async (
+  datasetId: string,
+  revision: Revision,
+  lang: string,
+  start?: 0,
+  end?: 100
+) => {
+  const fileService = getFileService();
+  const stream = await fileService.loadStream(`${revision.id}_${lang}.parquet`, datasetId);
+
+  const chunks = [];
+  for await (const chunk of stream) {
+    chunks.push(chunk);
+  }
+  const buffer = Buffer.concat(chunks);
+  const arrayBuffer = buffer.buffer.slice(buffer.byteOffset, buffer.byteOffset + buffer.byteLength);
+
+  const asyncBuffer = {
+    byteLength: arrayBuffer.byteLength,
+    slice: async (start: number | undefined, end: number | undefined) => arrayBuffer.slice(start, end)
+  };
+
+  const metadata = parquetMetadata(arrayBuffer);
+  const rows = await parquetReadObjects({ file: asyncBuffer, rowStart: start, rowEnd: end });
+
+  return { metadata, rows };
+};
