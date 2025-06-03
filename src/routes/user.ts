385fda61b75b03f58eb3f68d1db447092aa825b8
Phil Moorhouse <phil@wheelsandcogs.co.uk>
2025-06-03 15:20:40 +0100

    throw 404 if user not set

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -2,19 +2,20 @@ import { Request, Response, NextFunction, Router } from 'express';
 
 import { logger } from '../utils/logger';
 import { Locale } from '../enums/locale';
-import { UnknownException } from '../exceptions/unknown.exception';
 import { UserDTO } from '../dtos/user/user-dto';
 import { User } from '../entities/user/user';
+import { NotFoundException } from '../exceptions/not-found.exception';
 
 export const userRouter = Router();
 
 userRouter.get('/', async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    logger.info('Getting the current user');
-    const user = req.user as User;
-    res.json(UserDTO.fromUser(user, req.language as Locale));
-  } catch (error) {
-    logger.error(error, 'Error listing topics');
-    next(new UnknownException());
+  logger.info('Getting the current user...');
+  const user = req.user as User;
+
+  if (!user) {
+    next(new NotFoundException('errors.no_user'));
+    return;
   }
+
+  res.json(UserDTO.fromUser(user, req.language as Locale));
 });
