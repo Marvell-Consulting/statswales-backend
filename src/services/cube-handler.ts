37fc727058094b7ceff6cedee5cfd3d5a2e17e73
Jamie Maynard <jamie@punkyideas.com>
2025-09-19 18:32:26 +0100

    SW-993 Simplify cube builder and make things faster

--- src/services/cube-handler.ts
+++ src/services/cube-handler.ts
@@ -39,8 +39,7 @@ export const CORE_VIEW_NAME = 'core_view';
 export const createAllCubeFiles = async (
   datasetId: string,
   buildRevisionId: string,
-  buildType = CubeBuildType.FullCube,
-  buildId = crypto.randomUUID()
+  buildType = CubeBuildType.FullCube
 ): Promise<void> => {
   const datasetRelations: FindOptionsRelations<Dataset> = {
     factTable: true,
@@ -79,7 +78,7 @@ export const createAllCubeFiles = async (
   });
   logger.debug(`Build type = ${buildType}`);
 
-  const build = await BuildLog.startBuild(buildRevision, buildType, buildId);G
+  const build = await BuildLog.startBuild(buildRevision, buildType);
 
   const createBuildSchemaRunner = dbManager.getCubeDataSource().createQueryRunner();
   try {
