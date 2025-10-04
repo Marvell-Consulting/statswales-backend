afb4f374662d08f4d61f62830064d99c3b3dbdc7
Jamie Maynard <29251905+j-maynard@users.noreply.github.com>
2025-10-04 13:40:30 +0100

    Move from duckdb-async to duckdb-neo
    DuckDB Async has been depricated in favour of duckdb-neo and is no
    longer being updated.  For the long term support of the project its in
    our interests to move from async to neo.
    
    This change allowed us to go over the remaining duckdb calls and refine
    the its usage slightly.  There's now a single duckdb instance created
    and maintained in the app.  This allows us to allocate more resources to
    duckdb while at the sametime controlling duckdbs use of available
    container memory.  Another change is moving from duckdbs spacial plugin
    for Excel handling to the newer built in Excel plugin.
    
    Signed-off-by: Jamie Maynard <29251905+j-maynard@users.noreply.github.com>

--- src/services/duckdb.ts
+++ src/services/duckdb.ts
@@ -1,4 +1,4 @@
-import { Database } from 'duckdb-async';
+import { DuckDBConnection, DuckDBInstance } from '@duckdb/node-api';
 import { format as pgformat } from '@scaleleap/pg-format';
 
 import { logger as parentLogger } from '../utils/logger';
@@ -10,39 +10,49 @@ const logger = parentLogger.child({ module: 'DuckDB' });
 
 const config = appConfig();
 
-export const DUCKDB_WRITE_TIMEOUT = config.duckdb.writeTimeOut;
+let duckDBInstance: DuckDBInstance | undefined;
 
-export const safelyCloseDuckDb = async (quack: Database): Promise<void> => {
-  await quack.exec(`CHECKPOINT;`);
-  await quack.close();
-  return new Promise((f) => setTimeout(f, DUCKDB_WRITE_TIMEOUT));
-};
-
-export const duckdb = async (cubeFile = ':memory:'): Promise<Database> => {
+export const duckdb = async (cubeFile = ':memory:'): Promise<DuckDBConnection> => {
   const { threads, memory } = config.duckdb;
 
-  logger.debug(`Creating DuckDB instance with ${threads} thread(s) and ${memory} memory limit.`);
+  if (!duckDBInstance) {
+    logger.debug(`Creating DuckDB instance with ${threads} thread(s) and ${memory} memory limit.`);
+    duckDBInstance = await DuckDBInstance.create(':memory:', {
+      threads: threads.toString(),
+      memory_limit: memory,
+      default_block_size: '16384',
+      temp_directory: path.resolve(os.tmpdir(), 'duckdb_temp'),
+      preserve_insertion_order: 'false'
+    });
 
-  const duckdb = await Database.create(':memory:');
-
-  await duckdb.exec(pgformat('SET threads = %L;', threads));
-  await duckdb.exec(pgformat('SET memory_limit = %L;', memory));
-  await duckdb.exec("SET default_block_size = '16384';");
-  await duckdb.exec(`SET temp_directory='${path.resolve(os.tmpdir(), 'duckdb_temp')}';`);
-  await duckdb.exec('SET preserve_insertion_order=false;');
+    const setupConn = await duckDBInstance.connect();
+    try {
+      await linkToPostgresSchema(setupConn, 'data_tables');
+      await linkToPostgresSchema(setupConn, 'lookup_tables');
+    } catch (error) {
+      logger.fatal(error, 'Something went wrong trying to setup DuckDB postgres connections');
+      throw error;
+    } finally {
+      setupConn.disconnectSync();
+    }
+    logger.debug('Successfully set up duckDB instance');
+  } else {
+    logger.debug('Using existing duckdb instance');
+  }
+  const duckdb = await duckDBInstance.connect();
 
   if (cubeFile !== ':memory:') {
-    await duckdb.exec(pgformat('ATTACH %L AS cube_file;', cubeFile));
-    await duckdb.exec('USE cube_file;');
+    await duckdb.run(pgformat('ATTACH %L AS cube_file;', cubeFile));
+    await duckdb.run('USE cube_file;');
   }
-
+  logger.debug('Successfully connected to duckDB');
   return duckdb;
 };
 
-export const linkToPostgresSchema = async (quack: Database, schema: 'lookup_tables' | 'data_tables'): Promise<void> => {
+async function linkToPostgresSchema(quack: DuckDBConnection, schema: 'lookup_tables' | 'data_tables'): Promise<void> {
   logger.debug(`Linking to postgres ${schema} schema`);
-  await quack.exec(`LOAD 'postgres';`);
-  await quack.exec(`
+  await quack.run(`LOAD 'postgres';`);
+  await quack.run(`
     CREATE OR REPLACE SECRET (
       TYPE postgres,
       HOST '${config.database.host}',
@@ -52,6 +62,5 @@ export const linkToPostgresSchema = async (quack: Database, schema: 'lookup_tabl
       PASSWORD '${config.database.password}'
     );
   `);
-  await quack.exec(pgformat(`ATTACH '' AS %I (TYPE postgres, SCHEMA %L);`, `${schema}_db`, schema));
-  await quack.exec(pgformat(`USE %I;`, `${schema}_db`));
-};
+  await quack.run(pgformat(`ATTACH OR REPLACE '' AS %I (TYPE postgres, SCHEMA %L);`, `${schema}_db`, schema));
+}
