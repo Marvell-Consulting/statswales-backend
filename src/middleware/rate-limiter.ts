0ad856c2cdecf73cdabba80514017a025a140e0c
Phil Moorhouse <phil@wheelsandcogs.co.uk>
2025-10-08 13:10:24 +0100

    don't instantiate rateLimiter at all in CI

--- src/middleware/rate-limiter.ts
+++ src/middleware/rate-limiter.ts
@@ -1,5 +1,5 @@
 import { Request, Response, NextFunction } from 'express';
-import rateLimit from 'express-rate-limit';
+import rateLimit, { RateLimitRequestHandler } from 'express-rate-limit';
 
 import { appConfig } from '../config';
 
@@ -7,16 +7,18 @@ const config = appConfig();
 
 const bypass = (re: Request, res: Response, next: NextFunction): void => next();
 
-const limit = rateLimit({
-  windowMs: config.rateLimit.windowMs,
-  max: config.rateLimit.maxRequests,
-  standardHeaders: true,
-  legacyHeaders: false,
-  handler: (req, res) => {
-    res.status(429).json({
-      message: 'Too many requests, please try again later.'
-    });
-  }
-});
+const limit = (): RateLimitRequestHandler => {
+  return rateLimit({
+    windowMs: config.rateLimit.windowMs,
+    max: config.rateLimit.maxRequests,
+    standardHeaders: true,
+    legacyHeaders: false,
+    handler: (req, res) => {
+      res.status(429).json({
+        message: 'Too many requests, please try again later.'
+      });
+    }
+  });
+};
 
-export const rateLimiter = config.rateLimit.windowMs === -1 ? bypass : limit;
+export const rateLimiter = config.rateLimit.windowMs === -1 ? bypass : limit();
