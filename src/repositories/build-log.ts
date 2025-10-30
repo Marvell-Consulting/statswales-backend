52a3e0fa76075f001c35617af0f6f37a0689e66e
Jamie Maynard <jamie@punkyideas.com>
2025-10-30 17:33:56 +0000

    Fixes an issue with builds when a revision is empty
    The fix for incomplete incomplete builds broke times when revisions are
    empty from a cube build point of view but we still need a cube based on
    the details from the previous revision.
    
    Added the build id to the returned object from the rebuild end points.
    
    Finally fixed the SQL logic for checking for active bulk builds.

--- src/repositories/build-log.ts
+++ src/repositories/build-log.ts
@@ -1,17 +1,13 @@
 import { dataSource } from '../db/data-source';
 import { BuildLog } from '../entities/dataset/build-log';
 import { CubeBuildType } from '../enums/cube-build-type';
-import { Not } from 'typeorm';
+import { In, Not } from 'typeorm';
 import { CubeBuildStatus } from '../enums/cube-build-status';
 
 export const BuildLogRepository = dataSource.getRepository(BuildLog).extend({
   async getAllActiveBulkBuilds(): Promise<BuildLog[]> {
     return BuildLog.find({
-      where: [
-        { type: CubeBuildType.AllCubes },
-        { type: CubeBuildType.DraftCubes },
-        { status: Not(CubeBuildStatus.Completed) }
-      ]
+      where: { type: In([CubeBuildType.AllCubes, CubeBuildType.DraftCubes]), status: Not(CubeBuildStatus.Completed) }
     });
   },
 
