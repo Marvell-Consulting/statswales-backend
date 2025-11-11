80705b01c38bc8f9001b85e7716ddfc7fc218a15
Jamie Maynard <jamie@punkyideas.com>
2025-11-11 16:52:54 +0000

    Validate data values are numeric
    Validates that the data values supplied by the publisher are all numeric
    values.  Now returns an exception when non-numeric values are detected
    with a list of offending values.

--- src/enums/fact-table-validation-exception-type.ts
+++ src/enums/fact-table-validation-exception-type.ts
@@ -8,6 +8,8 @@ export enum FactTableValidationExceptionType {
   FailedToLoadData = 'failed_to_load_data',
   DuplicateFact = 'duplicate_fact',
   IncompleteFact = 'incomplete_fact',
+  NoDataValueColumn = 'no_data_value_column',
+  NonNumericDataValueColumn = 'non_numeric_data_value',
   UnknownError = 'unknown_error',
   UnknownFileType = 'unknown_file_type',
   UnmatchedColumns = 'unmatched_columns'
