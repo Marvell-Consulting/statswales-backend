d0852ee2df357e74421cf075f6b547c775e66723
Jamie Maynard <29251905+j-maynard@users.noreply.github.com>
2026-01-11 23:26:14 +0000

    Add test pivot table test end point to V2 API

--- src/routes/consumer/v2/api.ts
+++ src/routes/consumer/v2/api.ts
@@ -10,7 +10,8 @@ import {
   getPublishedRevisionById,
   getPublishedDatasetData,
   getPublishedDatasetFilters,
-  generateFilterId
+  generateFilterId,
+  getPublishedDatasetPivot
 } from '../../../controllers/consumer-v2';
 import { NotFoundException } from '../../../exceptions/not-found.exception';
 import { PublishedDatasetRepository } from '../../../repositories/published-dataset';
@@ -94,5 +95,6 @@ publicApiV2Router.get('/:dataset_id/filters', ensurePublishedDataset, getPublish
 publicApiV2Router.post('/:dataset_id/data', ensurePublishedDataset, jsonParser, generateFilterId);
 publicApiV2Router.get('/:dataset_id/data', ensurePublishedDataset, getPublishedDatasetData);
 publicApiV2Router.get('/:dataset_id/data/:filter_id', ensurePublishedDataset, getPublishedDatasetData);
+publicApiV2Router.get('/:dataset_id/data/:filter_id/pivot', ensurePublishedDataset, getPublishedDatasetPivot);
 
 // publicApiV2Router.post('/:dataset_id/pivot', loadPublishedDataset(), getPostgresPivotTable);
