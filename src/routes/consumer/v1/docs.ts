aa0b5169c74ed07f2ce4c41fa1a4917ef0709a2b
Phil Moorhouse <phil@wheelsandcogs.co.uk>
2026-04-21 15:35:39 +0100

    enable Welsh api docs

--- src/routes/consumer/v1/docs.ts
+++ src/routes/consumer/v1/docs.ts
@@ -1,23 +1,22 @@
 import { Router } from 'express';
 
 import enSpec from './openapi-en.json';
-// TODO: Re-enable Welsh docs once human translations are ready
-// import cySpec from './openapi-cy.json';
+import cySpec from './openapi-cy.json';
 import { config } from '../../../config';
 
 export const apiDocRouter = Router();
 
 // Replace the placeholder in the OpenAPI spec with the actual backend URL
 const consumerApiSpecEn = JSON.parse(JSON.stringify(enSpec).replaceAll('{{backendURL}}', config.backend.url));
-// const consumerApiSpecCy = JSON.parse(JSON.stringify(cySpec).replaceAll('{{backendURL}}', config.backend.url));
+const consumerApiSpecCy = JSON.parse(JSON.stringify(cySpec).replaceAll('{{backendURL}}', config.backend.url));
 
 apiDocRouter.get('/swagger.json', (_req, res) => {
   res.json(consumerApiSpecEn);
 });
 
-// apiDocRouter.get('/swagger-cy.json', (_req, res) => {
-//   res.json(consumerApiSpecCy);
-// });
+apiDocRouter.get('/swagger-cy.json', (_req, res) => {
+  res.json(consumerApiSpecCy);
+});
 
 // swagger-ui-express uses a shared singleton for swagger-ui-init.js, so multiple swaggerUi.setup()
 // calls in the same app overwrite each other. Redirect to the combined /docs/ UI instead,
