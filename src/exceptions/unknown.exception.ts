d92fce87fec23b057415d5abe8f1e81b75b83980
Jamie Maynard <jamie@punkyideas.com>
2026-05-06 13:21:20 +0100

    Remove unused cube build result

--- src/exceptions/unknown.exception.ts
+++ src/exceptions/unknown.exception.ts
@@ -1,7 +1,4 @@
-import { CubeBuildResult } from '../dtos/cube-build-result';
-
 export class UnknownException extends Error {
-  performance: CubeBuildResult | null = null;
   constructor(
     public message = 'Server Error',
     public status = 500
